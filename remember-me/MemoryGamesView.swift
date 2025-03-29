9d61285683a2b3557080e32b0dc660d9ee614500
Xiaoya Zou <84748829+xiaoyazz@users.noreply.github.com>
2025-03-29 01:04:02 -0400

    Update logo

---
+++ remember-me/MemoryGamesView.swift
@@ -0,0 +1,18 @@
+//
+//  MemoryGamesView.swift
+//  remember-me
+//
+//  Created by Xiaoya Zou on 2025-03-29.
+//
+
+import SwiftUI
+
+struct MemoryGamesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoryGamesView()
+}
