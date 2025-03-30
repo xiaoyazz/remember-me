6e6c0bf44bd47852498e1a560b2bf510c5b046fd
Xiaoya Zou <84748829+xiaoyazz@users.noreply.github.com>
2025-03-29 20:35:46 -0400

    Updaye My Memory Games view

--- remember-me/MemoryGamesView.swift
+++ remember-me/MemoryGamesView.swift
@@ -7,13 +7,46 @@
 
 import SwiftUI
 
-// test by xiaoya
-
 struct MemoryGamesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section(header: Text("Family & Pets")) {
+                    NavigationLink(destination: Text("Comming soon...")) {
+                        Label("Faces I Love", systemImage: "photo")
+                    }
+                    
+                    NavigationLink(destination: Text("Coming soon...")) {
+                        Label("My Furry Friends", systemImage: "pawprint")
+                    }
+                }
+                
+                Section(header: Text("Life")) {
+                    NavigationLink(destination: Text("Coming soon...")) {
+                        Label("Where I live", systemImage: "building.2")
+                    }
+
+                    NavigationLink(destination: Text("Coming soon...")) {
+                        Label("This Is Me", systemImage: "book.closed")
+                    }
+                }
+                
+                Section(header: Text("Entertainment")) {
+                    NavigationLink(destination: Text("Coming soon...")) {
+                        Label("Musical Memories", systemImage: "music.note")
+                    }
+
+                    NavigationLink(destination: Text("Coming soon...")) {
+                        Label("My Sports Days", systemImage: "sportscourt")
+                    }
+                }
+                
+            }
+            .navigationTitle("Memory Games")
+        }
     }
 }
+
 
 #Preview {
     MemoryGamesView()
