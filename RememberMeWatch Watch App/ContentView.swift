02bdb25de02807142a7941f8f0daf9ccd0ee8c26
Xiaoya Zou <84748829+xiaoyazz@users.noreply.github.com>
2025-03-30 04:31:43 -0400

    Add watch connectivity

---
+++ RememberMeWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RememberMeWatch Watch App
+//
+//  Created by Xiaoya Zou on 2025-03-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ remember-me/HealthDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  HealthDataManager.swift
+//  remember-me
+//
+//  Created by Xiaoya Zou on 2025-03-30.
+//
+
+import Foundation
