52813f2ba009031e08df06208334253f41e83911
Xiaoya Zou <84748829+xiaoyazz@users.noreply.github.com>
2025-03-29 21:04:43 -0400

    Update faces i love view with images

---
+++ remember-me/FacesILoveView.swift
@@ -0,0 +1,18 @@
+//
+//  FacesILoveView.swift
+//  remember-me
+//
+//  Created by Xiaoya Zou on 2025-03-29.
+//
+
+import SwiftUI
+
+struct FacesILoveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FacesILoveView()
+}
