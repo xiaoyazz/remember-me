53a1798a20c3c6d4fe79e51dbed23ce0c5124f12
Xiaoya Zou <84748829+xiaoyazz@users.noreply.github.com>
2025-03-29 20:04:52 -0400

    Update daily planner and data model

--- remember-me/PlannerTask.swift
+++ remember-me/PlannerTask.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct PlannerTask: Identifiable, Codable {
+    let id: String // use String for MongoDB _id
+    let date: String 
+    let time: String
+    let description: String
+    let note: String?
+    let category: String?
+}
